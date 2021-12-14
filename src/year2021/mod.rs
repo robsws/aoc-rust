965511bf563f0e91c28b8793356813896fd94b30
Rob Streeting <robsws@gmail.com>
2021-12-14 09:18:01 +0000

    day 11 part 1

--- src/year2021/mod.rs
+++ src/year2021/mod.rs
@@ -1,5 +1,6 @@
 mod day1; mod day2; mod day3; mod day4; mod day5;
 mod day6; mod day7; mod day8; mod day9; mod day10;
+mod day11;
 
 /// Dispatch the correct function given the day and part
 /// given.
@@ -25,6 +26,8 @@ pub fn solve(day: u8, part: u8, input_file_path: &str) {
         (9, 2) => day9::part2(input_file_path),
         (10, 1) => day10::part1(input_file_path),
         (10, 2) => day10::part2(input_file_path),
+        (11, 1) => day11::part1(input_file_path),
+        // (11, 2) => day11::part2(input_file_path),
         _ => {
             eprintln!(
                 "Solution to 2015 day {} part {} not yet implemented.",
