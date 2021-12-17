a2dc1a486d892efa6b282ef988626a3a1307dd0e
Rob Streeting <robsws@gmail.com>
2021-12-17 20:57:21 +0000

    day 15 part 2

--- src/year2021/day15.rs
+++ src/year2021/day15.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use crate::{input_file::read_lines, data_structs::{Coord, Grid, MinPriorityQueue}};
 
 pub fn part1(input_file_path: &str) {
@@ -8,7 +10,11 @@ pub fn part1(input_file_path: &str) {
 }
 
 pub fn part2(input_file_path: &str) {
-
+    let lines = read_lines(input_file_path);
+    let grid = parse_lines(lines);
+    let grid = expand_grid(&grid);
+    let lowest_risk = djikstra_path(grid);
+    println!("{}", lowest_risk);
 }
 
 fn parse_lines(lines: Vec<String>) -> Grid<u8> {
@@ -17,6 +23,41 @@ fn parse_lines(lines: Vec<String>) -> Grid<u8> {
         c.to_digit(10).unwrap_or_else(|| panic!("Unable to parse character to int: {}", c)) as u8
     ).collect();
     Grid::<u8>::with_elements(lines[0].len(), lines.len(), elements)
+}
+
+fn expand_grid(grid: &Grid<u8>) -> Grid<u8> {
+    let mut new_grid = Grid::new(grid.xsize * 5, grid.ysize * 5, 0);
+    for tile_x in 0..5 {
+        for tile_y in 0..5 {
+            let sub_grid = raise_risk(&grid, tile_x + tile_y);
+            splice_grid(
+                &mut new_grid, 
+                &sub_grid, 
+                tile_x as usize * grid.xsize, 
+                tile_y as usize * grid.ysize
+            );
+        }
+    }
+    new_grid
+}
+
+fn raise_risk(grid: &Grid<u8>, amount: u8) -> Grid<u8> {
+    let mut new_grid = Grid::new(grid.xsize, grid.ysize, 0);
+    for x in 0..grid.xsize {
+        for y in 0..grid.ysize {
+            let new_risk = (grid.get(x, y) + amount - 1) % 9 + 1;
+            new_grid.set(x, y, new_risk);
+        }
+    }
+    new_grid
+}
+
+fn splice_grid(big_grid: &mut Grid<u8>, small_grid: &Grid<u8>, offset_x: usize, offset_y: usize) {
+    for x in 0..small_grid.xsize {
+        for y in 0..small_grid.ysize {
+            big_grid.set(x + offset_x, y + offset_y, *small_grid.get(x, y));
+        }
+    }
 }
 
 fn djikstra_path(grid: Grid<u8>) -> u32 {
