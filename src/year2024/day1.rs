6b511e097ff6cf69d2423f796f4f9b3b9283736b
rostre <rostre@fastmail.com>
2024-12-01 21:00:06 +0000

    feat: day 1 part 2 2024

--- src/year2024/day1.rs
+++ src/year2024/day1.rs
@@ -1,6 +1,6 @@
 use crate::input_file::read_lines;
 
-use std::iter::zip;
+use std::{collections::HashMap, iter::zip};
 
 pub fn part1(input_file_path: &str) {
     let lines = read_lines(input_file_path);
@@ -11,6 +11,18 @@ pub fn part1(input_file_path: &str) {
 
 pub fn part2(input_file_path: &str) {
     let lines = read_lines(input_file_path);
+    let (left_list, right_list) = parse_input(lines);
+    let histogram = generate_histogram(&right_list);
+    let total_similarity: i32 =
+        left_list.into_iter()
+            .map(|x| {
+                match histogram.get(&x) {
+                    Some(n) => x * (*n as i32),
+                    None => 0,
+                }
+            })
+            .sum();
+    println!("{}", total_similarity);
 }
 
 fn parse_input(lines: Vec<String>) -> (Vec<i32>, Vec<i32>) {
@@ -42,4 +54,20 @@ fn total_distance(mut left_list: Vec<i32>, mut right_list: Vec<i32>) -> i32 {
     zip(left_list, right_list)
         .map(|(l, r)| (r-l).abs())
         .sum()
+}
+
+/// generate a histogram mapping of numbers to how often they appear in the list.
+fn generate_histogram(list: &Vec<i32>) -> HashMap<i32,usize> {
+    let mut histogram = HashMap::<i32,usize>::new();
+    for x in list {
+        match histogram.get_mut(x) {
+            Some(n) => {
+                *n += 1;
+            },
+            None => {
+                histogram.insert(*x, 1);
+            },
+        }
+    }
+    histogram
 }
